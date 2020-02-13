595e65d94bbba06cf7d922a43d1ad310a4cde927
kchang00 <k_chang@fanshaweonline.ca>
2020-02-13 17:32:15 -0500

    reorganized pan's files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'load.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <main>
+        <h1>Home</h1>
+        <a href="admin/index.php">Go to Login</a>
+    </main>
+</body>
+<?php include 'templates/footer.php';?>
+</html>
